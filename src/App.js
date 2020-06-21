a9baebe9fec030681a47e12b8715e40d676b3002
gautamrajdbest <me2xps@gmail.com>
2020-06-21 16:48:33 +0530

    "App.jsx modified"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
